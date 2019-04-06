cf367aeaf233b36dc803cf89634ef1ab8c92b676
Joe Andaverde <joe.andaverde@gmail.com>
2019-04-05 19:40:35 -0500

    Prebuilt osx

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-if (process.platform === "win32") {
-	module.exports = require('./prebuilt/win/Release/desktopIdle');
+const Fs = require("fs");
+
+if (Fs.existsSync("./build/Release/desktopIdle.node")) {
+  module.exports = require("./build/Release/desktopIdle");
+} else if (process.platform === "win32") {
+  module.exports = require("./prebuilt/win/Release/desktopIdle");
+} else if (process.platform === "darwin") {
+  module.exports = require("./prebuilt/osx/Release/desktopIdle");
 } else {
-    module.exports = require('./prebuilt/osx/Release/desktopIdle'); 
+  throw "desktopIdle node binding not found";
 }
